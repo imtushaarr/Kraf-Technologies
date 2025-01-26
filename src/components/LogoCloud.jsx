4be735ff1372d69d56aa6e43aa0c3ead3b3958d8
Tushar Gupta <tusharguptagps@gmail.com>
2025-01-26 22:04:09 +0530

    Updated

--- src/components/LogoCloud.jsx
+++ src/components/LogoCloud.jsx
@@ -3,37 +3,33 @@ import Logo2 from "../assets/comimgs/3 copy.png";
 import Logo4 from "../assets/comimgs/Project.png";
 
 export default function LogoCloud() {
-    return (
-      <div className="bg-black py-20 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-4xl font-semibold text-white">
-            Meets Our Brands
-          </h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            <img
-              alt="Transistor"
-              src={Logo1}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="Reform"
-              src={Logo2}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="SavvyCal"
-              src={Logo4}
-              width={158}
-              height={48}
-              className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            />
-          </div>
+  return (
+    <div className="bg-black py-12 sm:py-20">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <h2 className="text-center text-2xl sm:text-3xl font-semibold text-white">
+          Meets Our Brands
+        </h2>
+        <div className="mx-auto mt-8 grid max-w-lg grid-cols-2 items-center gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-3 sm:gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {/* Logo 1 */}
+          <img
+            alt="Brand 1"
+            src={Logo1}
+            className="col-span-1 max-h-10 sm:max-h-12 w-full object-contain"
+          />
+          {/* Logo 2 */}
+          <img
+            alt="Brand 2"
+            src={Logo2}
+            className="col-span-1 max-h-10 sm:max-h-12 w-full object-contain"
+          />
+          {/* Logo 3 */}
+          <img
+            alt="Brand 3"
+            src={Logo4}
+            className="col-span-1 max-h-10 sm:max-h-12 w-full object-contain"
+          />
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
